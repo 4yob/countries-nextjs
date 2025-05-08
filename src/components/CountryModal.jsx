10ae5c3ea0878a92f586829685018f43db5e2242
Alejandra Barros <alejandra.barros@aluno.senai.br>
2025-05-08 11:49:04 -0300

    feat: alter card style

--- src/components/CountryModal.jsx
+++ src/components/CountryModal.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import styles from "../styles/CountryModal.module.css";
 import Image from "next/image";
+import { Card } from "antd";
 
 export default function CountyModel({ country, onClose }) {
     return (
         <div className={styles.overlay} onClick={onClose}>
-            <div className={styles.modal} onClick={(e) => e.stopPropagation()}>
+            <Card className={styles.modal} onClick={(e) => e.stopPropagation()}>
                 <h2>{country.translations.por.common}</h2>
                 <Image src={country.flags.png} alt={`Bandeira de ${country.translations.por.common}`} width={100} height={100} className={styles.flag} />
                 <p>Nome Oficial: {country.translations.por.official}</p>
@@ -14,7 +15,7 @@ export default function CountyModel({ country, onClose }) {
                 <p>Sub-região: {country.subregion || "Não tem"}</p>
                 <p>População: {country.population.toLocaleString()}</p>
                 <p>Fuso Horário: {country.timezones[0]}</p>
-            </div>
+            </Card>
         </div>
     );
 }
